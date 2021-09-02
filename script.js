10741d6254a1a67e9596b77e08c7704781548bff
leo011619 <leonard.ferraz@hotmail.com>
2021-09-02 20:09:11 -0300

    f01/estudando as variaveis

---
+++ script.js
@@ -0,0 +1,15 @@
+var nome = "Leonardo Ferraz";
+var id = 22;
+alert("Olá " + nome + " #" + id);
+console.log(nome + " tem " + id + " anos");
+
+var n1 = 5;
+var n2 = 10;
+
+console.log(n1 + n2);
+
+var frase = "Brasil é o campeão da copa!";
+
+console.log(frase.replace("Brasil", "França"));
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
