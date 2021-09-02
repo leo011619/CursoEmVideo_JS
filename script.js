f2e3a9206747d145c7e1035cca60408a24388987
leo011619 <leonard.ferraz@hotmail.com>
2021-09-02 20:45:01 -0300

    f02/revisando arrays e objetos

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-var nome = "Leonardo Ferraz";
-var id = 22;
-alert("Olá " + nome + " #" + id);
-console.log(nome + " tem " + id + " anos");
+//var nome = "Leonardo Ferraz";
+// var id = 22;
+// alert("Olá " + nome + " #" + id);
+// console.log(nome + " tem " + id + " anos");
 
-var n1 = 5;
-var n2 = 10;
+// var n1 = 5;
+// var n2 = 10;
 
-console.log(n1 + n2);
+// console.log(n1 + n2);
 
-var frase = "Brasil é o campeão da copa!";
+// var frase = "Brasil é o campeão da copa!";
 
-console.log(frase.replace("Brasil", "França"));
-console.log(frase.toLowerCase());
-console.log(frase.toUpperCase());
+// console.log(frase.replace("Brasil", "França"));
+// console.log(frase.toLowerCase());
+// console.log(frase.toUpperCase());
+
+// var lista = ["banana", "maça", "pera"];
+// console.log(lista);
+// alert("Minha fruta favorita é a " + lista[0]);
+// lista.push("uva");
+// lista.pop(uva)
+// console.log(lista);
+// console.log(lista.toString());
+// console.log(lista.toString()[2]);
+// console.log(lista.join(" - "));
+
+// var fruta = { nome: "banana", cor: "amarela" };
+// console.log(fruta);
+// alert(fruta.nome + " tem a cor " + fruta.cor);
+
+var frutas = [
+  { nome: "banana", cor: "amarela" },
+  { nome: "maça", cor: "vermelha" },
+  { nome: "pera", cor: "verde" },
+];
+console.log(frutas);
+alert("A " + frutas[01].nome + " é da cor " + frutas[01].cor);
