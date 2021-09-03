9f8d75acbbf3959174af2e31b33716b96adbf7ee
leo011619 <leonard.ferraz@hotmail.com>
2021-09-03 14:27:55 -0300

    f04/ revisando funções básicas

--- script.js
+++ script.js
@@ -55,6 +55,30 @@
 // for (count = 0; count <= 5; count++);
 // alert(count);
 
-var d = new Date();
+// var d = new Date();
 
-alert(d.getFullYear());
+// alert(d.getFullYear());
+
+// function soma(n1, n2) {
+//   return n1 + n2;
+// }
+
+// alert(soma(5, 10));
+
+// function setReplace(frase, nome, novoNome) {
+//   return frase.replace(nome, novoNome);
+// }
+
+// alert(setReplace("Vai Japão!", "Japão", "Brasil"));
+
+function validaIdade(idade) {
+  var validar;
+  if (idade >= 18) {
+    validar = true;
+  } else {
+    validar = false;
+  }
+  return validar;
+}
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
