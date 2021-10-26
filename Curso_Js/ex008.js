f2e82f5ba203a197b1d5d252ab13e0100cdd60ae
leo011619 <leonard.ferraz@hotmail.com>
2021-10-26 14:15:49 -0300

    Aula 11 - exercício 8: Condições pt1 - condições simples

---
+++ Curso_Js/ex008.js
@@ -0,0 +1,6 @@
+var vel = 20;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if (vel > 60) {
+  console.log(`Você ultrapasssou o limite de velocidade permitido. MULTADO!`);
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
