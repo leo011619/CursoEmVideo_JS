5e7f8eadcecff288d96b50db0f4f8ba7fb017fe0
leo011619 <leonard.ferraz@hotmail.com>
2021-10-28 18:17:40 -0300

    Aula 13 e 14: While, Do While e For

---
+++ Curso_Js/While e Do While.js
@@ -0,0 +1,13 @@
+var c = 1;
+
+/*só While
+while (c < 6) {
+  console.log("Vai ficar tudo bem!");
+  c++;
+}*/
+
+//Do e While
+do {
+  console.log(`Volta número ${c} da corrida`);
+  c++;
+} while (c <= 6);
